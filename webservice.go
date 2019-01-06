9ff3e71623116c8d76bbe51a0057975328aed03b
Poomrat Boonyawong <poomrat.b@linecorp.com>
2019-01-06 11:14:44 +0700

    properly seed random

--- webservice.go
+++ webservice.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math/rand"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	captcha "github.com/poomrat/gocaptcha"
@@ -16,6 +17,7 @@ func main() {
 	})
 
 	r.GET("/captcha", func(c *gin.Context) {
+		rand.Seed(time.Now().UTC().UnixNano())
 		c.String(200, captcha.Generate(rand.Intn(2)+1, rand.Intn(10), rand.Intn(3)+1, rand.Intn(10)))
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
